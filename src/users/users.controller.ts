056b77f587bf5f1d2ae45a19c4b10071c3a7d790
ANASS BENDARSI <bendarsi@gmail.com>
2021-07-01 10:10:53 +0100

    core(auth): auth guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Session } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guards';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { SignUpUserDto } from './dtos/sign-up-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -10,6 +18,12 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Get()
+  @UseGuards(AuthGuard)
+  hello() {
+    return 'hello';
+  }
+
   @Post('signup')
   async signUp(
     @Body() body: SignUpUserDto,
@@ -19,5 +33,10 @@ export class UsersController {
     console.log(session);
     session.userId = user.id;
     return user;
+  }
+
+  @Post('signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
   }
 }
